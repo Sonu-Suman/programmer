23ccfae90d23f549bdeae15a5ec78e24c24ddd5c
Sonu Suman <sumankumar86028@gmail.com>
2021-01-19 19:40:09 +0530

    This is guessing game program.

---
+++ return.py
@@ -0,0 +1,30 @@
+import random
+
+name = input("What's your name: ")
+
+print(f'Hey, {name}! This is guessing game ')
+print("Do you play this game")
+
+answer = input("Enter you answer(y/n)?: ")
+
+if answer == "y":
+    print("let's play!")
+else:
+    print("OK, We meet again")
+    quit()
+
+n = 0
+computer = random.randint(1, 10)
+
+while n <= 3:
+    num = int(input("Enter your guessing number: "))
+
+    if computer < num:
+        print("you might choosing big number from computer")
+    elif computer > num:
+        print("you might choosing small number from computer")
+    n += 1
+
+    if computer == num:
+        print("You guess the correct number")
+        break
