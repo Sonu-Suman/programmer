afb8b63b78140127ff92a9e5be9975907c89ede8
Raushan Suman <sumankumar86028@gmail.com>
2021-01-25 00:59:39 +0530

    I have attached second type of A.T.M. machine code(Binery.py)

--- Binery.py
+++ Binery.py
@@ -1,3 +1,5 @@
+# <======This is the first type of A.T.M. code=======>
+
 this = " l.S. Bank"
 name = input("Enter your name.\nWho is using this card please enter your name:  ")
 
@@ -59,4 +61,66 @@ while chance >= 0:
         chance = chance - 1
         if chance == 0:
             print("\nNo more tries")
+    break
+
+
+# <=======This is second type of A.T.M. code=======>
+
+Bank_name = "L.S. Bank"
+
+name = input("What's your name: ")
+print(f'Hi {name}, Thank you for using {Bank_name}')
+
+count = 0
+Balance = 100000000
+
+while count < 3:
+    password = 4598
+    while count < 3:
+        passu = input("Enter your password: ")
+        if int(passu) == password:
+            print("Thank you")
+            print("You Enter for: ")
+            print("Checking for balance:  1")
+            print("Withdraw money:  2")
+            print("Deposit cash:  3")
+            print("Collecting your card:  4(>3)")
+
+            try:
+                enter = int(input("Enter your choosing number: "))
+
+                if enter == 1:
+                    print(f'Your balance in bank account:\n  {Balance}$')
+
+                elif enter == 2:
+                    try:
+                        money = int(input("Hi sir!\n Please, Enter your withdrawal money: "))
+                        if money < Balance:
+                            print(f'Thank you {name}, You currently collect {money}$')
+                            Balance -= money
+                            print(f'{name} currently balance: {Balance}$')
+                        else:
+                            print(f'Sorry sir, Your total amount is less than {money}$')
+                            print("Thank you sir")
+                    except:
+                        print("Please! Enter valid money")
+
+                elif enter == 3:
+                    try:
+                        deposit = int(input("Enter your deposit money: "))
+                        Balance += deposit
+                    except:
+                        print("Please! Enter valid money")
+                    print(f'Hello {name}, Your currently balance: {Balance}$')
+
+                elif enter > 3:
+                    print(f'Thanks for using {Bank_name}')
+                    break
+            except:
+                print("Please, Enter your valid input")
+
+        else:
+            print("You typed answer is incorrect")
+            print("Please, Enter a valid password")
+        count += 1
     break
