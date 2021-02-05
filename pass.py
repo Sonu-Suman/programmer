5c3e731bf375f344384c7fc25b43e9311c9d3baf
Raushan Suman <sumankumar86028@gmail.com>
2021-02-06 01:25:28 +0530

    added more comment in the python file

--- pass.py
+++ pass.py
@@ -1,8 +1,11 @@
+# import module
 import string as s
 from random import *
 
-
+# make some type of password 
 ch = s.ascii_letters + s.digits + s.punctuation
 
+# let's define number of password charecter
 password = "".join(choice(ch) for x in range(randint(8, 12)))
+# print the password
 print(password)
