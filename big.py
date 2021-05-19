d2b7633070c542e8534a157d98a5cecaf2ad3f50
Aparajita suman <sumankumar86028@gmail.com>
2021-05-19 21:47:46 +0530

    I am added new program for finding the biggest number in the array

--- big.py
+++ big.py
@@ -1,6 +1,11 @@
+# Finding the biggest number in array not uisng max function
+
+# This is only applicable for small array and list:
+# This is not for big array
+
 lst = []
 i = 0
-
+"""
 while i < 3:
     num = int(input("Enter your number for list: "))
     lst.append(num)
@@ -18,3 +23,19 @@ else:
         print(lst[1], " is the biggest number in this list")
     else:
         print(lst[2], " is the biggest number in the list")
+"""
+
+# This is also applicable for big array
+
+
+
+def findbiggestNumber(arr):
+    biggestnumber = arr[0]
+
+    for i in range(1, len(arr)):
+        if arr[i] > biggestnumber:
+            biggestnumber = arr[i]
+
+    print('This is the biggest number in your array: ', biggestnumber)
+
+findbiggestNumber([43, -23, 23, 95, 234, 230, 809])
