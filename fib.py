3036d291340c87068d3b3f5a3b6832ef7c8fbd68
Aparajita suman <sumankumar86028@gmail.com>
2021-08-19 13:17:06 +0530

    This is for finding fibbonacci number.

--- fib.py
+++ fib.py
@@ -1,6 +1,7 @@
 # This is the first Approach and it's take lots of time for finding result.
 # This is based upon Reccurssion methode
 
+# 1)
 class Sol:
     def fib_2(self, n):
         assert n>=0 and int(n) == n, 'Please enter the postive integer input.'
@@ -17,10 +18,19 @@ s1 = Sol()
 print(s1.fib_2(40))
 # This takes around 40-60 sec for finding result
 
+# 2).
+
+def fib(n):
+    if n<=1:
+        return n
+
+    return fib(n-1)+fib(n-2)
+
 
 # This is the Seocnd Approach and it's takes much less time
-# This solution based upon Dynamic data structure method
+# This solution based upon Dynamic Programming data structure method
 
+# 1).
 class Solution:
     def fib(self, n, memo):
         assert n>=0 and int(n) == n, 'Please enter the postive integer input.'
@@ -44,3 +54,16 @@ class Solution:
 
 s = Solution()
 print(s.fib_memo(40))
+
+
+# 2).
+
+def fib(n):
+    f = {}
+    f[0] = 0
+    f[1] = 1
+
+    for i in range(2, n+1):
+        f[i] = f[i-1]+f[i-2]
+
+    return f[n]
