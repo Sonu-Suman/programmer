88b04f7f5ac040d29e7c93a443972dc3acd70c1f
Aparajita suman <sumankumar86028@gmail.com>
2022-03-13 14:44:29 +0530

    Adding one more code for fib.

--- fib.py
+++ fib.py
@@ -67,3 +67,27 @@ def fib(n):
         f[i] = f[i-1]+f[i-2]
 
     return f[n]
+
+
+# For Memory limit
+
+def solve(A):
+    fib = [0] * 3
+    if A<=1:
+        fib[0] = 0
+        fib[1] = 1
+        return fib[A]
+    else:
+        fib[0] = 0
+        fib[1] = 1
+        
+        i = 1
+        while i<=A:
+            fib[0] = fib[1]
+            fib[1] = fib[2]
+            fib[2] = (fib[0]+fib[1])
+            i +=1
+        
+        return fib[2]
+
+print(solve(100000))
